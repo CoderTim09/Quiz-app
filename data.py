59593f5ba002686de3ecfc1fe84e31d40c0162ab
Timmis <109297977+CoderTim09@users.noreply.github.com>
2024-07-18 17:43:04 +0200

    initial commit

---
+++ data.py
@@ -0,0 +1,37 @@
+import requests
+
+parameters = {
+    "amount": 10,
+    "type": "boolean"
+}
+
+response = requests.get("https://opentdb.com/api.php", params= parameters)
+response.raise_for_status()
+data = response.json()
+question_data = data["results"]
+
+
+
+# Exempel
+# question_data = [
+#     {
+#         "category": "Science: Computers",
+#         "type": "boolean",
+#         "difficulty": "medium",
+#         "question": "The HTML5 standard was published in 2014.",
+#         "correct_answer": "True",
+#         "incorrect_answers": [
+#             "False"
+#         ]
+#     },
+#     {
+#         "category": "Science: Computers",
+#         "type": "boolean",
+#         "difficulty": "medium",
+#         "question": "The first computer bug was formed by faulty wires.",
+#         "correct_answer": "False",
+#         "incorrect_answers": [
+#             "True"
+#         ]
+#     },
+#     
